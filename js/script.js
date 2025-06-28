752d3e27a23292a2253d565737196bd56699f788
Kamil Szurlej <kamilszurlej123@gmail.com>
2025-06-28 12:45:54 +0200

    5.3 remove active class

--- js/script.js
+++ js/script.js
@@ -5,10 +5,18 @@ function titleClickHandler(){
     console.log(event);
 
     //remove class 'active' from all links
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
 
     //add class 'active' to clicked link
 
-    //remove class 'active from all articles
+    //remove class 'active' from all articles
+    const activeArticles = document.querySelectorAll('.posts article');
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
 
     //get 'href' attribute from the clicked link
 
