9f57a0d579c620ba0d0d38ca2bef65c5757c32de
Kamil Szurlej <kamilszurlej123@gmail.com>
2025-06-28 13:09:23 +0200

    Finish article preview

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
-function titleClickHandler(){
+function titleClickHandler(event){
+    event.preventDefault();
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
 
@@ -11,6 +13,9 @@ function titleClickHandler(){
     }
 
     //add class 'active' to clicked link
+    clickedElement.classList.add('active');
+    console.log('clickedElement: ', clickedElement);
+    console.log('clickedElement: ' + clickedElement);
 
     //remove class 'active' from all articles
     const activeArticles = document.querySelectorAll('.posts article');
@@ -19,11 +24,15 @@ function titleClickHandler(){
     }
 
     //get 'href' attribute from the clicked link
+    const clickedElementHref = clickedElement.getAttribute('href');
+    console.log(clickedElementHref);
 
     //find the correct article using the selector (value of 'href' attribute)
-
+    const targetArticle = document.querySelector(clickedElementHref);
+    console.log(targetArticle);
+    
     //add class 'active' to the correct article
-
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
